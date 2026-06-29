52d78e4107fee9b1f6e014366244834055299bae
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-29 11:31:28 +0300

    kullanıcı tanımlılar part 2

--- 04_functions/2_user_defined.py
+++ 04_functions/2_user_defined.py
@@ -59,4 +59,48 @@ hesapla_topla, hesapla_carp = hesapla(3,9)
 print(f"hesapla_topla: {hesapla_topla}")
 print(f"hesapla_carp: {hesapla_carp}")
 
+# varsayılan parametre
 
+def selam(isim,mesaj = "Merhaba") :
+    print(f"{isim} {mesaj}")
+    
+selam("Kaan Hocam")
+selam("Hazal","Selam")
+
+
+#keyword argümanı
+"""
+    # Docstring
+    # Description: bu fonksiyon selamlama yapar.
+    # Input: 
+    #     isim (str): kullanıcının ismi
+    #     yas (int): kullanıcının yaşı
+    #     meslek, 
+    #     c, 
+    #     lr, 
+    #     epoch
+    # Output: None
+"""
+def selam(isim, yas, meslek, c, lr, epoc) :
+    print(isim, yas, meslek, c, lr, epoc)
+    
+selam( isim = "Hazal", yas = 34, meslek = "Software Developer", c = "0.1", lr = "0.001", epoc = "1000")
+
+# type hint (modern python)
+def topla (a : int, b : int) -> int :
+    return a + b
+
+toplam = topla(3,4)
+
+print(f"toplam = {toplam}")
+
+# nested function -fonk. içinde fonk. kullanımı
+
+def kare(x) :
+    kare = x ** 2 # x * x
+    return kare
+
+def yazdir(x) :
+    print(kare(x))
+
+yazdir(5)
